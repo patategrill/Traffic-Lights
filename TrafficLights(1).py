c7e736cdce35e2212578b3f8bb0f6be05a2d427b
patategrill <noahrichard35850@gmail.com>
2026-04-20 12:44:58 +0200

    Add traffic light functionality with radio control
    Implement traffic light control with green and red phases.

---
+++ TrafficLights(1).py
@@ -0,0 +1,43 @@
+from microbit import *
+import radio 
+
+radio.on()
+radio.config(group=17)
+display.clear()
+#définit la constante losange
+losange = Image('00900:'
+                '09990:'
+                '90909:'
+                '09990:'
+                '00900:')
+#définit la constante losange_complet
+losange_complet = Image('00900:'
+                        '09990:'
+                        '99999:'
+                        '09990:'
+                        '00900:')
+def feu_vert():
+    for _ in range(9):
+         sleep(1000)
+         display.show(Image.ARROW_N)
+    sleep(1000)
+    display.show(losange)
+    sleep(1000)
+    display.show(losange_complet)
+
+def feu_rouge():
+    for i in range(5,-1,-1):
+          display.show(Image.NO)
+          sleep(1000)
+          display.show(i)
+          sleep(1000)
+
+def init() :
+        feu_vert()
+        feu_rouge()
+        
+while True :
+    message = radio.receive()
+    if message == "init":
+        init()
+    sleep(10)
