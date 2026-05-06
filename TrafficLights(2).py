daf0bcae6ecbc9e49dba8839abce49c86fda2acb
patategrill <194829329+patategrill@users.noreply.github.com>
2026-05-06 18:23:35 +0200

    Update TrafficLights(2).py

--- TrafficLights(2).py
+++ TrafficLights(2).py
@@ -1,6 +1,6 @@
 from microbit import *
 import radio
-
+init = False
 radio.on()
 radio.config(group=17)
 croix = (Image( '90009:'
@@ -8,6 +8,16 @@ croix = (Image( '90009:'
                 '00900:'
                 '09090:'
                 '90009'))
+losange = (Image( '00900'
+                  '09990'
+                  '90909'
+                  '09990'
+                  '00900'))
+losange_complet = (Image( '00900'
+                          '09990'
+                          '9999²9'
+                          '09990'
+                          '00900'))
 def feu_rouge():
     for i in range(5,-1,-1):
         """affiche le décompte du feu"""
@@ -25,14 +35,12 @@ def feu_vert():
     sleep(1000)
     display.show(losange_complet)
 
-def init() :
-        radio.send("init")
-        sleep(10)
-        feu_rouge()
-        feu_vert()
+init = True
 
-if button_a.is_pressed(): 
-    while True:
-        message = radio.receive()
-        if message == "go":
-            init()
+while True:
+    if button_a.is_pressed(): 
+        while True:
+            radio.send("init")
+            sleep(10)
+            feu_rouge()
+            feu_vert()
