71484a80be979dc7136c92c3ae7d6ac34913d327
patategrill <194829329+patategrill@users.noreply.github.com>
2026-05-10 15:01:55 +0200

    Refactor traffic light images and display functions
    Updated image variables and display logic for traffic lights.

--- TrafficLights(2).py
+++ TrafficLights(2).py
@@ -1,41 +1,51 @@
 from microbit import *
 import radio
+
 init = False
+
 radio.on()
 radio.config(group=17)
-croix = (Image( '90009:'
+display.clear()
+
+cross = (Image( '90009:'
                 '09090:'
                 '00900:'
                 '09090:'
                 '90009'))
-losange = (Image( '00900'
+
+diamond = (Image( '00900'
                   '09990'
                   '90909'
                   '09990'
                   '00900'))
-losange_complet = (Image( '00900'
+
+complete_diamond = (Image( '00900'
                           '09990'
                           '9999²9'
                           '09990'
                           '00900'))
+
 def feu_rouge():
     for i in range(5,-1,-1):
         """affiche le décompte du feu"""
-        display.show(croix)
+        display.show(cross)
         sleep(1000)
         display.show(i)
         sleep(1000)
+
 
 def feu_vert():
     for _ in range(9):
          sleep(1000)
          display.show(Image.ARROW_N)
     sleep(1000)
-    display.show(losange)
+    display.show(diamond)
     sleep(1000)
-    display.show(losange_complet)
+    display.show(complete_diamond)
+
 
 init = True
+
 
 while True:
     if button_a.is_pressed(): 
